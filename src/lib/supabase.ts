52f499e5f31ddbeca7cb2b269eb7fa1d8a2cf9d7
jankalivoda-hue <jan.kalivoda@rohlik.cz>
2026-04-02 13:49:04 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Tyto údaje nahraď těmi, které jsi získal v kroku 3
-const supabaseUrl = 'TVOJE_PROJECT_URL';
-const supabaseAnonKey = 'TVŮJ_ANON_PUBLIC_KEY';
+const supabaseUrl = 'https://qemmkkenjsuuhshcdhwl.supabase.co';
+const supabaseAnonKey = 'sb_publishable_BIgAzoo0W9bA7F00iC5CFg_3VKtvGrg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
