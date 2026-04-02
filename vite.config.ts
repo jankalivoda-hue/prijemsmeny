094feeda0c4294df26ff674ab72bc25ab6487ea9
jankalivoda-hue <jan.kalivoda@rohlik.cz>
2026-04-02 12:17:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/prijemsmeny/', // <--- TENTO ŘÁDEK JE KLÍČOVÝ PRO GITHUB PAGES
   server: {
     host: "::",
     port: 8080,
